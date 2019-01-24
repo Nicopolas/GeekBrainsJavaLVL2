595a40fa2c9d1619c10ce930cf769b14ef9310d2
slepdog@gmail.com <79Gi0123>
2019-01-24 11:54:47 +0300

    homework4 (v1.0)

--- src/com/company/ChatClient.java
+++ src/com/company/ChatClient.java
@@ -2,6 +2,7 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 /*
 1. Создать окно для клиентской части чата: большое текстовое поле для отображения
 переписки в центре окна.
@@ -13,33 +14,84 @@ import java.awt.*;
  */
 
 public class ChatClient extends JFrame {
-    private JToggleButton sendBtn;
-    private JTextArea bigField;
-    private JTextField smallField;
+    private JTextField textMessageField;
+    private JTextArea correspondenceDisplayField;
+    private JButton sendMessageBtm;
+    private String textFieldMessageStr = "Введите ваше сообщение: ";
 
-    public ChatClient() throws HeadlessException {
-        super("Клиентская часть чата");
+    public ChatClient() {
+        setSize(600, 500);
+        setTitle("Клиентская часть чата");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        bigField = new JTextArea(15,15);
-        bigField.setToolTipText("большое текстовое поле для отображения\n" +
+        //Поле для отображения переписки
+        correspondenceDisplayField = new JTextArea();
+        correspondenceDisplayField.setToolTipText("Большое текстовое поле для отображения\n" +
                 "переписки в центре окна");
+        correspondenceDisplayField.setEditable(false);
+        correspondenceDisplayField.setLineWrap(true);
+        JScrollPane jsp = new JScrollPane(correspondenceDisplayField);
+        add(jsp, BorderLayout.CENTER);
 
-        smallField = new JTextField(5);
-        smallField.setToolTipText("Однострочное текстовое поле для ввода сообщений");
+        //Поле ввода сообщения
+        textMessageField = new JTextField(textFieldMessageStr);
+        textMessageField.setForeground(Color.GRAY);
+        textMessageField.setToolTipText("Однострочное текстовое поле для ввода сообщений");
 
-        sendBtn =  new JToggleButton("Отправить", false);
+        //Кнопка отправить
+        sendMessageBtm = new JButton("Отправить");
 
-        // Размещение эллементов в окне
-        Box contents = new Box(BoxLayout.Y_AXIS);
-        contents.add(new JScrollPane(bigField));
-        contents.add(smallField);
-        contents.add(sendBtn);
+        //Добавляем панель внизу окна
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.add(sendMessageBtm, BorderLayout.EAST);
+        bottomPanel.add(textMessageField, BorderLayout.CENTER);
 
-        // Вывод окна на экран
-        setContentPane(contents);
-        setSize(500, 400);
+        // вешаем слушателя на кнопку "Оправить"
+        sendMessageBtm.addActionListener(e ->
+                sendMessage()
+        );
+
+        //оправка сообщения по нажатию Enter
+        textMessageField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    sendMessage();
+                }
+            }
+        });
+
+        // добавляем placeholder для поля текстового поля
+        // пришлось писать самому если в JTextField они и есть то я не нашел
+        textMessageField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (textMessageField.getText().equals(textFieldMessageStr)) {
+                    textMessageField.setText("");
+                    textMessageField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (textMessageField.getText().isEmpty()) {
+                    textMessageField.setForeground(Color.GRAY);
+                    textMessageField.setText(textFieldMessageStr);
+                }
+            }
+        });
+
         setVisible(true);
+    }
+
+    //метод отправки сообщения
+    public void sendMessage() {
+        if (!(textMessageField.getText().trim().isEmpty() || textMessageField.getText().equals(textFieldMessageStr))) {
+            correspondenceDisplayField.append(textMessageField.getText() + "\n");
+            textMessageField.setText("");
+            textMessageField.grabFocus();
+        }
     }
 
     public static void main(String[] args) {
