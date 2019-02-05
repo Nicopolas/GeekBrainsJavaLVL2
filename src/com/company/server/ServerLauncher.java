5d935c8d6e9b392a1da12eb0546d7b1ea024cc6c
slepdog@gmail.com <slepdog@gmail.com>
2019-02-05 10:28:24 +0300

    homework7 (v0.001)

---
+++ src/com/company/server/ServerLauncher.java
@@ -0,0 +1,11 @@
+package com.company.server;
+
+/**
+ * @author ilnaz-92@yandex.ru
+ * Created on 01/02/2019
+ */
+public class ServerLauncher {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
